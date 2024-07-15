2ee2a56b6cd261286353293f621e36e37199c172
PATOOWORLD <info@patooworld.com>
2024-07-15 14:39:20 -0700

    Update middleware.ts
    remove unused codes and add used codes

--- middleware.ts
+++ middleware.ts
@@ -17,7 +17,7 @@ export const config = {
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
-  // Get hostname of request (e.g. demo.patoosite.net, demo.localhost:3000)
+  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   let hostname = req.headers
     .get("host")!
     .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
@@ -51,14 +51,14 @@ export default async function middleware(req: NextRequest) {
     );
   }
 
-  // special case for `patoosite.net` domain
-  if (hostname === "patoosite.net") {
+  // special case for `vercel.pub` domain
+  if (hostname === "vercel.pub") {
     return NextResponse.redirect(
       "https://vercel.com/blog/platforms-starter-kit",
     );
   }
 
-  // rewrite root application to `/home` folder
+  // rewrite root application to /home folder
   if (
     hostname === "localhost:3000" ||
     hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
